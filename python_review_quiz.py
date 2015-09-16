b67622d7091e977122473913a37e7fbba4082cc8
Dana <djmedhaug@gmail.com>
2015-09-16 14:40:33 -0700

    initial commit

---
+++ python_review_quiz.py
@@ -0,0 +1,49 @@
+
+# 1. Print “Hello World”
+Print "Hello World"
+
+# 2. Create a list called fruit that has Apples, Oranges and Bananas as values.
+fruit = ['Apples', 'Oranges', 'Bananas']
+
+# 3. Print the list.
+Print 'fruit'	
+
+# 4. Change Oranges to Grapes using the numeric list index.
+fruit.index(['Oranges'],'Grapes')
+
+# 5. Create a loop that prints out each item in the fruit list.
+	# I found this example online, I have no idea what %s is used for.
+for fruits in fruit:
+	print 'Do you want an %s' % fruit
+
+# 6. Create a dictionary called people that has another dictionary for each Bob (age 22), Carol (age 47) and Justin (age 14) with their name and age. 
+	# I'm not sure if this will work because 'name' and 'age' were repeated for each person.
+people = {'name':'Bob', 'age': 22, 'name':'Carol', 'age': 47, 'name':'Justin', 'age':14,}
+
+# 7. Create a function that takes two numbers (a and b) and prints the total value when they are added.
+	# This function says I have an indentation error when I test it.
+def num(a,b):
+	a = [9]
+	b = [7]
+	print(9+7)
+
+# 8. C17. Print the new credit score of your object.all your function with 5 and 5, 10 and 15 and 3 and 6.
+	# No clue...
+
+# 9. Create a function that takes user input of an integer and loops through printing that number plus 5 until it reaches 1000.
+ def oneth(x):
+    while x < 1000:
+        x += 5
+        print x
+# 10. Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+# 11. Create a class called customer that takes the customer name as a variable and is initialized with age, location of Washington and a credit score of 718.
+
+# 12. Create an object from your customer class with the name of Jessie.
+
+# 13. Print the name value of your object.
+print python_review_quiz.name
+
+# 14. Print the location of your object.
+# 15. Print the credit score of your object.
+# 16. Change the credit score of your object.
